90c4ab53c0a53dc073cecffc17b3a3641ad3f9af
shunsugai <sugaishun@gmail.com>
2018-01-08 00:03:47 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+ruby '2.4.3'
+
+source 'https://rubygems.org'
+
+gem 'mini_racer'
---
+++ try_mini_racer.rb
@@ -0,0 +1,5 @@
+require 'mini_racer'
+
+context = MiniRacer::Context.new
+context.eval 'var adder = (a,b)=>a+b;'
+puts context.eval 'adder(20,22)'
